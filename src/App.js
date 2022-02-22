03e9b8f42935752a43bad0b28ce6164756ede208
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-02-22 13:35:37 +0700

    header components

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Home from './components/Home'
 import About from './components/About'
 import Product from './components/Product'
 import Contact from './components/Contact'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
-        <Route to='/' component={<Home />} />
-        <Route to='/about' component={<About />} />
-        <Route to='/product' component={<Product />} />
-        <Route to='/contact' component={<Contact />} />
-        <Navigate to='/' />
+        <Route to='/' element={<Home />} />
+        <Route to='/about' element={<About />} />
+        <Route to='/product' element={<Product />} />
+        <Route to='/contact' element={<Contact />} />
+        {/* <Navigate to='/' /> */}
       </Routes>
+      <Footer />
     </>
   );
 }
