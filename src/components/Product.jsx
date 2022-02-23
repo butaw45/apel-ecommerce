c9afa5924d2ba7f924d26974db59a939f4e5a25e
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-02-23 11:12:04 +0700

    load product item

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Data from '../Data'
+import { Link } from 'react-router-dom'
 
 const Product = () => {
+
+    const cardItem = (item) => {
+        return (
+            <div className="card my-5 py-5" key={item.id} style={{ width: "18rem" }}>
+                <img src={item.img} className="card-img-top" alt={item.title} />
+                <div className="card-body text-center">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="lead">Rp. {item.price}</p>
+                    <Link to={`/product/${item.id}`} className="btn btn-outline-primary">Buy Now</Link>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div>Product</div>
+        <div>
+            <div className="container py-5">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <h1>Product</h1>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row justify-content-around">
+                    {Data.map(cardItem)}
+                </div>
+            </div>
+        </div>
     )
 }
 
