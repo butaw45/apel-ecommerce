1498b09c6e6ffb7ed6592ba119dd708240e9bbab
Muhammad Haris <kumantheas@gmail.com>
2022-03-05 18:28:10 +0700

    price number

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,42 +2,42 @@ const Data = [
     {
         id: 0,
         title: 'Samsung Galaxy A52 - Black',
-        price: '5.399.000',
+        price: 5399000,
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quidem molestiae illo accusamus vel? Animi veritatis labore quae quo magnam modi ex earum odio quaerat atque sapiente voluptate, repellendus quas.',
         img: '/assets/image/product1.jpg'
     },
     {
         id: 1,
         title: 'Samsung Galaxy S21 FE 5G - Lavender',
-        price: '9.999.000',
+        price: 9999000,
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus animi alias dolorum architecto impedit esse illo nisi? Quam dolores molestias hic, quisquam commodi unde quibusdam! Adipisci dolorum repellat aliquam sapiente.',
         img: '/assets/image/product2.jpg'
     },
     {
         id: 2,
         title: 'Samsung Galaxy A52s - Violet',
-        price: '6.499.000',
+        price: 6499000,
         desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas ipsum est et reiciendis. Nihil consectetur eaque, iure odio enim quasi nostrum dolor reiciendis quam repudiandae libero porro. Tempore, voluptate ducimus.',
         img: '/assets/image/product3.jpg'
     },
     {
         id: 3,
         title: 'Samsung Galaxy M52 - Light Blue',
-        price: '5.399.000',
+        price: 5399000,
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque repellat harum praesentium maxime ea dicta voluptate dignissimos perferendis hic! Eveniet, quisquam illum! A quia ex ratione fugiat natus, ducimus magnam?',
         img: '/assets/image/product4.jpg'
     },
     {
         id: 4,
         title: 'Samsung Galaxy A12 - White',
-        price: '2.499.000',
+        price: 2499000,
         desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem hic deleniti error eius non facere accusamus quis vero? Placeat officiis impedit rerum, nulla eligendi voluptatum earum quibusdam minima libero esse?',
         img: '/assets/image/product5.jpg'
     },
     {
         id: 5,
         title: 'Samsung Galaxy A72 - White',
-        price: '5.999.000',
+        price: 5999000,
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem exercitationem, facilis quibusdam eaque temporibus, ab tenetur nostrum magni ipsum quia, possimus quaerat optio eum voluptates aliquam excepturi illo aspernatur ratione.',
         img: '/assets/image/product6.jpg'
     },
